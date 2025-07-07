e8e0fa96d5426a7aeb426dc4d250e8a15ab748fc
aisosia <1vandalaone@gmail.com>
2025-07-07 20:57:04 +0300

    QRCodeGenerator.tsx

--- src/components/QRCodeGenerator.tsx
+++ src/components/QRCodeGenerator.tsx
@@ -4,7 +4,7 @@ export default function QRCodeGenerator({ value }: { value: string }) {
   return (
     <div className="mt-4">
       <h2 className="text-lg">QR-код для клиента</h2>
-      <QRCode value={value} size={200} />
+      <QRCode value={someValue} size={128} />
       <p className="break-all">{value}</p>
     </div>
   );
